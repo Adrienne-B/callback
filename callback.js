180915bb086e48d465e1885909da7d1a2116a402
adrienne_b <WorldofArts_95@yahoo.com>
2022-04-12 19:10:04 -0500

    callback assignment.

---
+++ callback.js
@@ -0,0 +1,8 @@
+//In my callback homework assignment, I decided to do a alarm clock-like callback function.
+//I passed my object which is morningReminder and/or 120000 which is 2 minutes converted into 120000 millseconds to my function which is as a paramenter
+
+const morningReminder = function() {
+    console.log("You have to get up so you won't be late to school for 8:00 pm");
+}
+morningRemindert();
+setTimeout(morningReminder, 120000);
